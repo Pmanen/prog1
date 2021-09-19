152f50112e6d9c5aa5824ce1afd532456260ce6b
Pepijn van Manen <pepijn@vanmanen.be>
2021-09-19 16:38:30 +0200

    bonus exercise - a bit unoptimised but it works well

--- KarelRevisited/src/nl/ru/ai/p4ai/exercise2/Path.java
+++ KarelRevisited/src/nl/ru/ai/p4ai/exercise2/Path.java
@@ -7,7 +7,63 @@ public class Path
   public static void main(String[] args)
   {
     map("path.map");
-    speed(100);
-    // your code here
+    speed(50);
+    checkFront();
+    while (!east())
+      turnRight();
+  }
+
+  public static void  checkFront(){
+    if (!inFrontOfWall())
+    {
+      step();
+      if (onBall())
+        checkFront();
+      else {
+        turn(2);
+        step();
+        turn(2);
+        checkLeft();
+      }
+    }
+    else
+      checkLeft();
+  }
+
+  public static void  checkLeft(){
+    turnLeft();
+    if (!inFrontOfWall()){
+      step();
+      if (onBall()) {
+        checkFront();
+      }
+      else {
+        turn(2);
+        step();
+        checkRight();
+      }
+    }
+    else{
+      turn(2);
+      checkRight();
+    }
+  }
+
+  public static void checkRight(){
+    if (!inFrontOfWall()) {
+      step();
+      if (onBall()){
+        checkFront();
+      }
+      else{
+        turn(2);
+        step();
+      }
+    }
+  }
+
+  public static void turn(int n){
+    while (n-- > 0)
+      turnRight();
   }
 }
