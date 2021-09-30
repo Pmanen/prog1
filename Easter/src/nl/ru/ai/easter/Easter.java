244e1d105afd9a0b83f605214c0b0635138fa0f2
Pepijn van Manen <pepijn@vanmanen.be>
2021-09-30 15:47:21 +0200

    added base files

---
+++ Easter/src/nl/ru/ai/easter/Easter.java
@@ -0,0 +1,56 @@
+package nl.ru.ai.easter;
+
+public class Easter
+{
+  static boolean isLeapYear(int year)
+  {
+    // implement this function
+    return false;
+  }
+
+  static int numberOfDaysInMonth(int year, Month month)
+  {
+    // implement this function
+    return 0;
+  }
+
+  static Month easterMonth(int year)
+  {
+    // implement this function
+    return Month.JANUARY;
+  }
+
+  static int easterDay(int year)
+  {
+    // implement this function
+    return 0;
+  }
+
+  static int dayNumberInYear(int day, Month month, int year)
+  {
+    // implement this function
+    return 0;
+  }
+
+  static Month monthInYearOfDayNumber(int dayNumber, int year)
+  {
+    // implement this function
+    return Month.JANUARY;
+  }
+
+  static int dayInMonthOfDayNumber(int dayNumber, int year)
+  {
+    // implement this function
+    return 0;
+  }
+
+  static void showHolyDays(int year)
+  {
+    // implement this function
+  }
+
+  public static void main(String[] arguments)
+  {
+    // implement this function
+  }
+}
